22595778a4bc1ac5bafdb5660df9e2b06df35dd6
M7moud_ <seekpotato47@gmail.com>
2025-07-15 22:28:19 +0300

    Update

--- src/main/java/dev/m7wq/qshopapi/annotations/Slot.java
+++ src/main/java/dev/m7wq/qshopapi/annotations/Slot.java
@@ -1,7 +1,7 @@
 package dev.m7wq.qshopapi.annotations;
 
 import com.avaje.ebean.validation.NotNull;
-import dev.m7wq.qshopapi.main.behaviors.Clickable;
+
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
